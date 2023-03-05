6320623d7239ed53cea503d05e8494283839ecc0
Paule Savier <116457108+paulesavier@users.noreply.github.com>
2023-03-05 21:09:03 +0300

    full program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,15 +11,43 @@ public class Main {
         animals.add(new Animal("lion", 3));
         animals.add(new Animal("whale", 4));
         animals.add(new Animal("dolphin", 5));
-        animals.add(new Animal("rhino", 6));
+        animals.add(new Animal("rhino", 11));
         animals.add(new Animal("tiger", 7));
-        animals.add(new Animal("elephant", 8));
-        animals.add(new Animal("parrot", 9));
+        animals.add(new Animal("elephant", 20));
+        animals.add(new Animal("parrot", 30));
         animals.add(new Animal("horse", 10));
 
-        Stream<Animal> stream = animals.stream();
-        animals.stream()
-                .filter(animal -> animal.getAge() > 5)
-                .forEach(animal -> System.out.println(animal.getName()));
+        Optional<Animal> isLion = animals.stream()
+                .filter(animal -> animal.getName().equals("lion"))
+                .findAny();
+
+        if (isLion.isPresent()) {
+            Animal lion = isLion.get();
+            System.out.println("Возраст льва: " + lion.getAge());
+        } else {
+            throw new IllegalArgumentException();
         }
+
+        long animalsOverFour = animals.stream()
+                .filter(animal -> animal.getAge() > 4)
+                .count();
+        System.out.println("Всего животных старше четырёх лет: " + animalsOverFour);
+
+        animals.stream()
+                .sorted(Comparator.comparingInt(animal -> animal.getAge()))
+                .forEach(animal -> System.out.println(animal.getName() + ", возраст: " + animal.getAge()));
+
+        Map<String, Animal> animalMap = animals.stream()
+                .collect(Collectors.toMap((animal -> animal.getName()), animal -> animal));
+        System.out.println(animalMap);
+
+        Optional<String> animalStr = animals.stream()
+                .map(animal -> animal.getName())
+                .reduce((prev, next) -> prev + ", " + next);
+
+        if (animalStr.isPresent()) {
+            String animalString = animalStr.get();
+            System.out.println(animalString);
+        }
+    }
 }
