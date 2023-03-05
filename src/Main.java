0955b98192c47debc19bc4897c6e4d01f5f20723
Paule Savier <116457108+paulesavier@users.noreply.github.com>
2023-03-05 21:15:21 +0300

    corrections

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
